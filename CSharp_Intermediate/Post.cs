9b468d8401b700fba7b173aa89afbe2d8789f67b
Susan Joshi <susan.joshi@aggrowth.com>
2020-06-09 11:41:44 -0500

    Design a StackOverflow Post

---
+++ CSharp_Intermediate/Post.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace CSharp_Intermediate
+{
+    public class Post
+    {
+        public Post(string title, string description)
+        {
+            this.Title = title;
+            this.Description = description;
+            this.CreatedDateTime = DateTime.Now;
+        }
+
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedDateTime { get; }
+
+        private int _upVoteCount;
+        private int _downVoteCount;
+
+        public void PrintPost()
+        {
+            Console.WriteLine($"Title: {Title} Description: {Description}");
+        }
+        
+        public void UpVote()
+        {
+            _upVoteCount++;
+        }
+
+        public void DownVote()
+        {
+            _downVoteCount++;
+        }
+
+        public int UpVoteCount()
+        {
+            return _upVoteCount;
+        }
+
+        public int DownVoteCount()
+        {
+            return _downVoteCount;
+        }
+
+        public int TotalVoteCount()
+        {
+            return _upVoteCount - _downVoteCount;
+        }
+
+    }
+}
--- CSharp_Intermediate/Program.cs
+++ CSharp_Intermediate/Program.cs
@@ -7,30 +7,16 @@ namespace CSharp_Intermediate
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Press enter to start the stopwatch");
-
-            Console.ReadLine();
-            
-            var watch = new StopWatch();
-
-            for (int i = 0; i < 3; i++)
-            {
-                Console.WriteLine("Start: " + watch.Start().Millisecond);
-
-                Thread.Sleep(2000);
-
-                Console.WriteLine("Start: " + watch.Stop().Millisecond);
-
-                Console.WriteLine("-----------");
-
-                Console.WriteLine("Duration: " +watch.GetDuration().Milliseconds);
-
-                Console.WriteLine("Press enter to start the stopwatch again");
-
-                Console.ReadLine();
-            }
-
-          
+           
+            var post = new Post("Hello", "World");
+            post.PrintPost();
+            post.UpVote();
+            post.UpVote();
+            post.UpVote();
+            post.DownVote();
+            post.DownVote();
+            Console.Write("You have " + post.UpVoteCount() + " up votes & ");
+            Console.Write("You have " + post.DownVoteCount() + " down votes");
 
         }
     }
